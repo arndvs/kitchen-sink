5e3c65ba461dbdfcb57329b72931f0491bca8204
Aaron Davis <aaron@ripemetrics.com>
2022-12-07 12:44:45 -0800

    added starter

---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout/Layout';
+import ButtonLink from '@/components/links/ButtonLink';
+import UnstyledLink from '@/components/links/UnstyledLink';
+import Seo from '@/components/Seo';
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+export default function HomePage() {
+  return (
+    <Layout>
+      {/* <Seo templateTitle='Home' /> */}
+      <Seo />
+
+      <main>
+        <section className='bg-white'>
+          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
+
+            <h1 className='mt-4'>
+              Next.js + Tailwind CSS + TypeScript + Redux + Material UI Starter
+            </h1>
+
+            <ButtonLink className='mt-6' href='/components' variant='light'>
+              See all components
+            </ButtonLink>
+
+            <UnstyledLink
+              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
+              className='mt-4'
+            >
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                width='92'
+                height='32'
+                src='https://vercel.com/button'
+                alt='Deploy with Vercel'
+              />
+            </UnstyledLink>
+
+            <footer className='absolute bottom-2 text-gray-700'>
+
+            </footer>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
